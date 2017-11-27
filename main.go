9f03d6b5d292b006c61a885a8a226d9e265f9562
Mehrdad Arshad Rad <arshad.rad@gmail.com>
2017-11-26 21:28:33 -0800

    fixed renamed package and loop

--- main.go
+++ main.go
@@ -3,14 +3,16 @@ package main
 import "C"
 
 import (
-	"github.com/mehrdadrad/gopyhttp/gohttp"
+	"github.com/mehrdadrad/pygohttp/gohttp"
 )
 
 //export HTTPRequest
-func HTTPRequest(url string) {
+func HTTPRequest(urls []string) {
 	var r []gohttp.Request
 
-	r = append(r, gohttp.Request{URL: url})
+	for _, url := range urls {
+		r = append(r, gohttp.Request{URL: url})
+	}
 
 	gohttp.Run(r)
 }
